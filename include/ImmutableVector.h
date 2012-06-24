bf14ea9f83618b6b0e1566467e499c457f2e1f97
Tim McIver <tim@timmciver.com>
2012-06-24 13:48:06 -0400

    Refactored ImmutableVector.h putting private member data at the top of class definition which defaults to private.

--- include/ImmutableVector.h
+++ include/ImmutableVector.h
@@ -2,12 +2,12 @@
 
 template <class T>
 class ImmutableVector {
+  T *m_data;
+  size_t m_size;
+
  public:
   ImmutableVector(T *a_buf, size_t a_size);
   ~ImmutableVector();
   size_t size() const;
   const T& operator[](size_t index) const;
- private:
-  T *m_data;
-  size_t m_size;
 };
